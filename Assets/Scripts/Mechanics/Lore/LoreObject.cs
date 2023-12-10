9adcc1ab28f8e54970e54cfc2c0b4db22d28ac1f
Nick <1025250@student.sae.edu.au>
2023-12-10 17:30:49 +0800

    Added Ending Manager

--- Assets/Scripts/Mechanics/Lore/LoreObject.cs
+++ Assets/Scripts/Mechanics/Lore/LoreObject.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class LoreObject : MonoBehaviour
 {
-    public bool isEnd;
     public int id;
 
 }
---
+++ Assets/Scripts/System/EndingManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine;
+
+public class EndingManager : MonoBehaviour
+{
+    [SerializeField] UnityEvent EndingOne;
+    [SerializeField] UnityEvent EndingTwo;
+    [SerializeField] UnityEvent EndingThree;
+}
