460a2dd73838b3960d1a6a5279f21ee756039b08
Nick <1025250@student.sae.edu.au>
2023-12-10 15:29:12 +0800

    Updated pause to slow down gameplay

--- Assets/Scripts/System/Pause.cs
+++ Assets/Scripts/System/Pause.cs
@@ -6,11 +6,17 @@ using UnityEngine.Events;
 public class Pause : MonoBehaviour
 {
     [SerializeField] UnityEvent PauseMenu;
-    private void Update()
+
+    public void Unpause()
+    {
+        Time.timeScale = 1;
+    }
+private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             PauseMenu.Invoke();
+            Time.timeScale = 0.000000000001f;
         }
     }
 }
