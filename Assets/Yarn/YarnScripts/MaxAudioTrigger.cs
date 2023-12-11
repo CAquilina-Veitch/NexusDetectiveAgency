7033cfa195fc271dbbeebb7c75feb43020cec7e3
Daniel B <1024175@student.sae.edu.au>
2023-12-11 19:04:41 +0800

    Fixed solo subtitles not continuing
    ...i hope

--- Assets/Yarn/YarnScripts/MaxAudioTrigger.cs
+++ Assets/Yarn/YarnScripts/MaxAudioTrigger.cs
@@ -9,6 +9,7 @@ public class MaxAudioTrigger : MonoBehaviour
     [SerializeField] EventInstance maxDiaSolo;
     [SerializeField] string maxDiaPath = "event:/Dialogue/EventMaxPuzzles";
     [SerializeField] private DialogueRunner dialogueRunner;
+    [SerializeField] LineView lineView;
     public int maxLineNumber;
 
     private void Awake()
@@ -22,5 +23,11 @@ public class MaxAudioTrigger : MonoBehaviour
         dialogueRunner.StartDialogue($"{maxLineNumber}");
         maxDiaSolo.setParameterByName("MaxPuzzles", maxLineNumber);
         maxDiaSolo.start();
+        StartCoroutine(NextDialogue());
+    }
+    IEnumerator NextDialogue()
+    {
+        yield return new WaitForSeconds(4f);
+        lineView.GetComponent<LineView>().OnContinueClicked();
     }
 }
