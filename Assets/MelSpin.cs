92592a3b4b19983778712a1d9580b7d5083703ff
Cody <1027166@student.sae.edu.au>
2023-12-12 02:47:23 +0800

    melspin 2

--- Assets/MelSpin.cs
+++ Assets/MelSpin.cs
@@ -8,9 +8,14 @@ public class MelSpin : MonoBehaviour
     {
         StartCoroutine(spinOnDelay());
     }
+    public GameObject[] toTurnOff;
     IEnumerator spinOnDelay()
     {
         yield return new WaitForSeconds(11.25f);
         transform.localRotation = Quaternion.EulerAngles(0,-90,0);
+        foreach(GameObject go in toTurnOff)
+        {
+            go.SetActive(false);
+        }
     }
 }
